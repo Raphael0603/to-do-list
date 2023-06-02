486177df02653f6e0b11f5a083cd0a3cdb116f8e
Raphael0603 <127576400+Raphael0603@users.noreply.github.com>
2023-06-02 05:56:43 -0700

    Add files via upload

---
+++ html/function.js
@@ -0,0 +1,33 @@
+
+function Add(){
+    let int = document.getElementById('input-1').value;
+    
+
+    if(int == ""){
+        alert("Please fill the blanks.");
+    }
+    else{
+        document.getElementById("none-flex").style.display = "flex";
+        // contents here, append, and more
+    }
+
+}
+
+// function newElement() {
+//     var li = document.createElement("li");
+//     var inputValue = document.getElementById("myInput").value;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+//     if (inputValue === '') {
+//       alert("You must write something!");
+//     } else {
+//       document.getElementById("myUL").appendChild(li);
+//     }
+//     document.getElementById("myInput").value = "";
+  
+//     var span = document.createElement("SPAN");
+//     var txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     li.appendChild(span);
+// }
